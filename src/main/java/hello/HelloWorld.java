743f9ae376ea8f4fba1dc3d82dd4aa04f964b136
upadhyaysac <sachin.dabbu@gmail.com>
2021-08-20 10:43:11 +0530

    Update HelloWorld.java
    reverted

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package hello;
 
 public class HelloWorld {
     public static String getMessage() {
-        return "Hello, World!"+ System.getenv("CATALINA_HOME");
+        return "Hello, World!");
     }
 }

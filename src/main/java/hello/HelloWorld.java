6fe5b08e8801e8c5f63c4748f132eb45aa42e6ea
upadhyaysac <sachin.dabbu@gmail.com>
2022-01-12 10:32:45 +0530

    Update HelloWorld.java
    test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package hello;
 
 public class HelloWorld {
     public static String getMessage() {
-        return "Hello,New World 8.0!";
+        return "Hello,New World from aws machine!";
     }
 }

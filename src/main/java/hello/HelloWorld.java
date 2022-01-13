044a7a97cce045acc0e98b6f73bf725d44d43991
upadhyaysac <sachin.dabbu@gmail.com>
2022-01-13 10:09:17 +0530

    Update HelloWorld.java
    test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package hello;
 
 public class HelloWorld {
     public static String getMessage() {
-        return "Hello,New World from jenkins 2!";
+        return "Hello,New World from jenkins 33!";
     }
 }
